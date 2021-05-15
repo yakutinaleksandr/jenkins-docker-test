41f766e03dd1a71e29311875197e4040c02cade3
Aliaksandr Yakutsin <yakutinaleksandr@gmail.com>
2021-05-15 19:09:50 +0300

    changes

--- src/test/java/com/example/dockerjenkins/service/impl/HelloServiceImplTest.java
+++ src/test/java/com/example/dockerjenkins/service/impl/HelloServiceImplTest.java
@@ -17,7 +17,7 @@ public class HelloServiceImplTest {
     @Test
     public void testSayHello() {
         // Given
-        final String expected = "hello";
+        final String expected = "hello1";
 
         // When
         final String actual = service.sayHello();
